6ef4ffbdc62346ea47fbb6d27c90f8826bd211a8
celinelorrain <celine.lorrain@edu.ece.fr>
2026-02-23 11:47:14 +0100

    Initial commit

---
+++ src/Client.java
@@ -0,0 +1,19 @@
+public class Client {
+    String nom;
+    String prenom;
+    String email;
+    String mdp;
+    String adresse;
+
+
+
+}
+
+class NouveauClient extends Client {
+
+}
+
+class AncienClient extends Client {
+
+
+}
---
+++ src/Hebergement.java
@@ -0,0 +1,13 @@
+public class Hebergement {
+    String identifiant;
+    String nom;
+    String adresse;
+    String type;
+    int nbMaxPersonne;
+    int prix;
+    String description;
+    String[] equipements;
+
+
+
+}
