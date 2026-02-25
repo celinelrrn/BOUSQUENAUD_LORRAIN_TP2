67735ba5a30d0f54676e49ff6e1533e2a352de89
Zoe Bousquenaud <zoe.bousquenaud@edu.ece.fr>
2026-02-25 18:15:26 +0100

    herbergement modif

--- src/Hebergement.java
+++ src/Hebergement.java
@@ -1,13 +1,28 @@
 public class Hebergement {
-    String identifiant;
-    String nom;
-    String adresse;
-    String type;
-    int nbMaxPersonne;
-    int prix;
-    String description;
-    String[] equipements;
+    //mettre en privé?? pour sécurité?? jsp
+    public String identifiant;
+    public String nom;
+    public String adresse;
+    public String type;
+    public int nbMaxPersonne;
+    public int prix;
+    public String description;
+    public String[] equipements;
 
+    //méthodes
+    //si dates libres renvoie 1 sinon 0
+    public int dates_libres(){
 
+        return 0;
+    }
 
+    public void Resume(){}//affcihe les infos?
+
+    public int prixTotal(){return 0;}
+
+    public void Add_dispo(){}
+
+    public void Suppr_dispo(){}
+
+    public void Add_note(){}
 }
