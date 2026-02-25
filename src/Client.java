26c8e529fa2e8586fc625d04b977ac35423f45e8
celinelorrain <celine.lorrain@edu.ece.fr>
2026-02-25 19:33:02 +0100

    constructeur client

--- src/Client.java
+++ src/Client.java
@@ -1,18 +1,37 @@
+import java.util.ArrayList;
+
 public class Client {
-    String nom;
-    String prenom;
-    String email;
-    String mdp;
-    String adresse;
-    String date_inscription;
+    private String nom;
+    private String prenom;
+    private String email;
+    private String mdp;
+    private String adresse;
+    private String date_inscription;
+
+    // constructeur
+    public Client (String nom, String prenom, String email, String mdp, String adresse, String date_inscription) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.email = email;
+        this.mdp = mdp;
+        this.adresse = adresse;
+        this.date_inscription = date_inscription;
+    }
+
 
 }
 
 class NouveauClient extends Client {
+    public NouveauClient(String nom, String prenom, String email, String mdp, String adresse, String date_inscription) {
+        super(nom, prenom, email, mdp, adresse, date_inscription);
+    }
 
 }
 
 class AncienClient extends Client {
-    Reservation[] liste_reservation;
+    ArrayList<Reservation> reservations;
 
+    public AncienClient(String nom, String prenom, String email, String mdp, String adresse, String date_inscription) {
+        super(nom, prenom, email, mdp, adresse, date_inscription);
+    }
 }
